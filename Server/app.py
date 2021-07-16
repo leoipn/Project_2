ff63d1297f6159cf90f668bb5cb2787982a20f91
Estrada <79946978+andrebestrada@users.noreply.github.com>
2021-07-15 23:09:24 -0500

    Update app.py

--- Server/app.py
+++ Server/app.py
@@ -4,19 +4,18 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-mongo_db = "travel_db"
+mongo_db = "Spotifydb"
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri=f'mongodb://localhost:27017/{mongo_db}')
 
-hello_dict = {"Hello": "World!"}
-
+# hello_dict = {"Hello": "World!"}
 
 @app.route("/songs")
 def jsonified():
     #songs = mongo.db.destinations.find({})
     #print(songs)
-    print([i for i in mongo.db.destinations.find({})])
-    songs = [i for i in mongo.db.destinations.find({})]
+    print([i for i in mongo.db.Top200.find({})])
+    songs = [i for i in mongo.db.Top200.find({})]
     
     for song in songs:
         song.pop("_id")
