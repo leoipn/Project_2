98db2adb9dc819ba546edb5543bffb1d3489f3c8
Leo Preciado <leoipn@gmail.com>
2021-07-15 21:56:34 -0500

    Spotify Project - Flask Server

---
+++ Server/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+from flask_pymongo import PyMongo
+
+
+app = Flask(__name__)
+
+mongo_db = "travel_db"
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri=f'mongodb://localhost:27017/{mongo_db}')
+
+hello_dict = {"Hello": "World!"}
+
+
+@app.route("/songs")
+def jsonified():
+    #songs = mongo.db.destinations.find({})
+    #print(songs)
+    print([i for i in mongo.db.destinations.find({})])
+    songs = [i for i in mongo.db.destinations.find({})]
+    
+    for song in songs:
+        song.pop("_id")
+    
+    return jsonify(songs)
+    #return jsonify([i for i in mongo.db.destinations.find({})])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
