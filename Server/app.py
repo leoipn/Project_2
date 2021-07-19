784c19aac19b41e547b109b3f77b078c0a43d8f7
ANDRES ESTRADA <andrebestrada@gmail.com>
2021-07-18 23:57:15 -0500

    Clean and order ETL process, with API call and final data base fulfilled, added first concept of overview graphs and both principal json Data Sets added to Resources folder

--- Server/app.py
+++ Server/app.py
@@ -8,22 +8,66 @@ mongo_db = "Spotifydb"
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri=f'mongodb://localhost:27017/{mongo_db}')
 
-# hello_dict = {"Hello": "World!"}
 
 @app.route("/songs")
 def jsonified():
-    #songs = mongo.db.destinations.find({})
-    #print(songs)
-    print([i for i in mongo.db.Top200.find({})])
     songs = [i for i in mongo.db.Top200.find({})]
-    
+
     for song in songs:
         song.pop("_id")
     
-    
-    return jsonify(songs)
-    #return jsonify([i for i in mongo.db.destinations.find({})])
+    response = jsonify(songs)
+    response.headers.add('Access-Control-Allow-Origin', '*')
 
+    return response
+    
+@app.route("/streamsByDate")
+def jsonified2():
+    streams_by_date = [i for i in mongo.db.StreamsByDate.find({})]
+
+    for streams in streams_by_date:
+        streams.pop("_id")
+
+    response = jsonify(streams_by_date)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route("/streamsByArtist")
+def jsonified3():
+    streams_by_artist = [i for i in mongo.db.StreamsByArtist.find({})]
+
+    for streams in streams_by_artist:
+        streams.pop("_id")
+
+    response = jsonify(streams_by_artist)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route("/streamsByTrack")
+def jsonified4():
+    streams_by_track = [i for i in mongo.db.StreamsByTrack.find({})]
+
+    for streams in streams_by_track:
+        streams.pop("_id")
+
+    response = jsonify(streams_by_track)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route("/artist")
+def jsonified5():
+    artists = [i for i in mongo.db.SpotifyArtists.find({})]
+
+    for artist in artists:
+        artist.pop("_id")
+    
+    response = jsonify(artists)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Static/js/app.js
+++ Static/js/app.js
@@ -1,15 +1,83 @@
-
 // d3.json("http://127.0.0.1:5000/songs").then((importedData)=>{
 //     var songs = importedData;
 //     console.log(songs);
-
 // });
 
 console.log("Its working")
 
-d3.json("Resources/songs.json").then(importedData=>{
+// Group By JS Function
+var groupBy = function(xs, key) {
+    return xs.reduce(function(rv, x) {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+};
+
+function filter_first(x,data) {
+    data = data.slice(0, x)
+    return data
+};
+
+
+d3.json("http://127.0.0.1:5000/songs").then(importedData=>{
+
     var songs = importedData;
-    console.log(songs);
+    console.log(filter_first(1000,songs));
+
+    // console.log(groupBy(songs,"Artist"))
+});
+
+
+d3.json("http://127.0.0.1:5000/streamsByDate").then(importedData=>{
+    var streamsByDate = importedData;
+    // .toString();
+    var date = (streamsByDate.map(sample=>sample.MonthYear))
+    var streams = streamsByDate.map(sample=>sample.Streams)
+
+    var trace1 = {
+        x:date,
+        y:streams,
+        type: 'scatter'
+    }
     
-    console.log("Its alive")
+    var data = [trace1]
+
+    Plotly.newPlot('StreamsByDatePlot',data)
+});
+
+d3.json("http://127.0.0.1:5000/streamsByArtist").then(importedData=>{
+    var streamsByArtist = filter_first(20,importedData);
+    // console.log(streamsByArtist);
+
+    // .toString();
+    var artist = (streamsByArtist.map(sample=>sample.Artist));
+    var streams = streamsByArtist.map(sample=>sample.Streams);
+
+    var data = [
+        {
+          x: artist,
+          y: streams,
+          type: 'bar'
+        }
+      ];
+      
+      Plotly.newPlot('StreamsByArtistPlot', data);
+});
+
+d3.json("http://127.0.0.1:5000/streamsByTrack").then(importedData=>{
+    var streamsByTrack = filter_first(10,importedData);
+    // console.log(streamsByTrack);
+    // .toString();
+    var track = (streamsByTrack.map(sample=>sample.Track))
+    var streams = streamsByTrack.map(sample=>sample.Streams)
+
+    var data = [
+        {
+          x: track,
+          y: streams,
+          type: 'bar'
+        }
+      ];
+      
+      Plotly.newPlot('StreamsByTrackPlot', data);
 });
